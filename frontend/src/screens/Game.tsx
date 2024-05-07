eea5daf3776e7cc8cbed336500dd71665f81435b
Harsh <harshchauhan6176@gmail.com>
2024-05-07 19:16:09 +0530

    first commit

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,77 +1,7 @@
-import { useEffect, useState } from "react";
-import { Button } from "../components/Button";
-import { ChessBoard } from "../components/ChessBoard";
-import { useSocket } from "../hooks/useSockte";
-import { Chess, Color } from "chess.js";
-
 export const INIT_GAME = "init_game";
 export const MOVE = "move";
 export const GAME_OVER = "game_over";
 
 export default function Game() {
-  const socket = useSocket();
-  const [chess, setChess] = useState<Chess | null>(new Chess());
-  const [board, setBoard] = useState(chess?.board());
-  const [started, setStarted] = useState(false);
-  const [color, setColor] = useState<Color | null>(null);
-
-  useEffect(() => {
-    if (!socket) return;
-
-    socket.onmessage = (ev) => {
-      const message = JSON.parse(ev.data);
-
-      switch (message.type) {
-        case INIT_GAME:
-          setChess(new Chess());
-          setBoard(chess?.board());
-          setColor(message.payload.color);
-          return;
-
-        case MOVE:
-          chess?.move(message.payload);
-          setBoard(chess?.board());
-          return;
-
-        case GAME_OVER:
-          console.log("Game over");
-          return;
-      }
-    };
-  }, [socket, chess]);
-
-  if (!socket) {
-    return <div>Connecting...</div>;
-  }
-  return (
-    <div className="justify-center flex">
-      <div className="grid grid-cols-6 gap-4 w-full">
-        <div className="col-span-4  w-full flex justify-center">
-          <ChessBoard
-            board={board}
-            socket={socket}
-            chess={chess}
-            setBoard={setBoard}
-            color={color}
-          />
-        </div>
-        <div className="col-span-2  w-full">
-          {!started && (
-            <Button
-              onClick={() => {
-                socket.send(
-                  JSON.stringify({
-                    type: INIT_GAME,
-                  })
-                );
-                setStarted(true);
-              }}
-            >
-              Play
-            </Button>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,36 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { Button } from "../components/Button.tsx";
-
 export default function Landing() {
-  const navigate = useNavigate();
-  return (
-    <>
-      <div className="flex justify-center">
-        <div className="pt-8 max-w-screen-lg">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <div className="flex justify-center">
-              <img src="/board.jpeg" alt="Chess board" />
-            </div>
-            <div className="pt-16">
-              <div className="flex justify-center">
-                <h1 className="text-4xl font-bold text-white">
-                  Play chess online with Harsh Chauhan.
-                </h1>
-              </div>
-
-              <div className="mt-8 flex justify-center">
-                <Button
-                  onClick={() => {
-                    navigate("/game");
-                  }}
-                >
-                  Play Online
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 }
